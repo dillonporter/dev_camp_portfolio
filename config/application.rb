d773a85decc3cd2ba090c2066cf03329494b6218
dillonporter <dillonporter@gmail.com>
2018-02-16 11:27:21 -0700

    Updated secret key base call

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ Bundler.require(*Rails.groups)
 module DevCampPortfolio
   class Application < Rails::Application
     config.eager_load_paths << "#{Rails.root}/lib"
-    # Initialize configuration defaults for originally generated Rails version.
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
     config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
